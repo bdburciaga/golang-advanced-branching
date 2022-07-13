a4368f822d3199c4f7eada3b3b4624aaceb91ddd
bnieto000 <Bradley_Burciaga@comcast.com>
2022-07-13 16:37:01 -0600

    Updates

--- vehicle.go
+++ vehicle.go
@@ -84,6 +84,18 @@ func main() {
 	generateRating()
 
 	// Print ratings for the different vehicles
+	for _, veh := range inventory {
+		switch v := veh.(type) {
+		case car:
+			v.carDetails()
+		case bike:
+			v.bikeDetails()
+		case truck:
+			v.truckDetails()
+		default:
+			fmt.Printf("Are you sure this Vehicle Type exists")
+		}
+	}
 }
 
 func readJSONFile() Values {
@@ -140,8 +152,9 @@ func generateRating() {
 }
 
 func showRating(model string) {
-	ratingFound := false
+
 	for m, r := range vehicleResult {
+		ratingFound := false
 		if m == model {
 			fmt.Printf("Total Ratings:%v\tPositive:%v\tNegative:%v\tNeutral:%v", r.feedbackTotal, r.feedbackPositive, r.feedbackNegative, r.feedbackNeutral)
 			ratingFound = true
@@ -155,4 +168,14 @@ func showRating(model string) {
 func (c *car) carDetails() {
 	fmt.Printf("\n%-5v: %-8v: %-12v \", \"Car\" , c.make, c.model")
 	showRating(c.model)
+}
+
+func (b *bike) bikeDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v \", \"Bike\" , b.make, b.model")
+	showRating(b.model)
+}
+
+func (t *truck) truckDetails() {
+	fmt.Printf("\n%-5v: %-8v: %-12v \", \"Truck\" , t.make, t.model")
+	showRating(t.model)
 }
